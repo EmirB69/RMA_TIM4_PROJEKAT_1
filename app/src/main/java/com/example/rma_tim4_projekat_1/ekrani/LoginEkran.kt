03b0a4c724f09a84e14715cf4641a7a197beb412
EmirB69 <emir.bajramovic2021@treca-gimnazija.edu.ba>
2024-04-05 18:06:22 +0200

    Projekat

--- app/src/main/java/com/example/rma_tim4_projekat_1/ekrani/LoginEkran.kt
+++ app/src/main/java/com/example/rma_tim4_projekat_1/ekrani/LoginEkran.kt
@@ -8,9 +8,12 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.OutlinedTextField
@@ -19,7 +22,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.fontResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -29,15 +34,16 @@ import javax.security.auth.login.LoginException
 
 @Composable
 fun LoginEkran(){
-    Column (verticalArrangement = Arrangement.Top, horizontalAlignment = Alignment.CenterHorizontally)
+
+    Column (verticalArrangement = Arrangement.Top, horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.offset(y = (30).dp))
     {
         Image(contentDescription = "Slika", painter = painterResource(id = R.drawable.logo),
-            modifier = Modifier.size(180.dp) )
+            modifier = Modifier.size(150.dp) )
 
-        Spacer(modifier = Modifier.height(20.dp))
+        Spacer(modifier = Modifier.height(70.dp))
         Box{
             Column {
-                Text(text = "ADRESA E-POSTE", fontWeight = FontWeight.Bold, fontSize = 25.sp)
+                Text(text = "ADRESA E-POSTE", fontFamily = jomhuriaFontFamily, fontWeight = FontWeight.Bold, fontSize = 25.sp)
                 OutlinedTextField(value = "", onValueChange = { }, singleLine = true)
             }
         }
@@ -46,38 +52,49 @@ fun LoginEkran(){
 
         Box{
             Column {
-                Text(text = "LOZINKA", fontWeight = FontWeight.Bold, fontSize = 25.sp)
+                Text(text = "LOZINKA", fontFamily = jomhuriaFontFamily, fontWeight = FontWeight.Bold, fontSize = 25.sp)
                 OutlinedTextField(value = "", onValueChange = {}, singleLine = true)
             }
         }
 
-        Spacer(modifier = Modifier.height(150.dp))
+        Spacer(modifier = Modifier.height(140.dp))
 
         Box{
-            Column (){
-                Text(text = "Zaboravili ste lozinku?", fontWeight = FontWeight.Bold, fontSize = 18.sp)
+            Column (modifier = Modifier.offset(y = (-60).dp)){
+                Text(
+                    text = "Zaboravili ste lozinku?",
+                    fontFamily = jomhuriaFontFamily,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 25.sp,
+                    modifier = Modifier.offset(x = (-50).dp),
+                )
             }
         }
 
         Column(
-            modifier = Modifier.fillMaxSize(),
+            //modifier = Modifier.fillMaxSize(),
+            modifier = Modifier.offset(y = (-60).dp),
             verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
+            horizontalAlignment = Alignment.CenterHorizontally,
+
         )
         {
             Button(
                 onClick = {},
-                border = BorderStroke(3.dp, Color.Black),
-                colors = ButtonDefaults.outlinedButtonColors(contentColor = Color.Black)
-
+                border = BorderStroke(5.dp, Color.Black),
+                colors = ButtonDefaults.outlinedButtonColors(contentColor = Color.White ),
+                modifier = Modifier.width(300.dp).height(100.dp),
+                shape = RoundedCornerShape(4.dp)
             )
             {
 
-                Text(text = "Login", color = Color.Black)
+                Text(text = "LOGIN", fontFamily = jomhuriaFontFamily, color = Color.Black, fontSize = 40.sp)
             }
         }
-
-
     }
-
 }
+
+val jomhuriaFontFamily = FontFamily(
+    Font(R.font.jomhuria)
+)
+
